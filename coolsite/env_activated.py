1673c315cefd94363ecf51cff173a53e60b9724e
DmitryLurk <redalert337596@gmail.com>
2022-12-01 16:47:50 +0400

    add envact

---
+++ coolsite/env_activated.py
@@ -0,0 +1,3 @@
+import os
+print(os.system("cd C:\\Users\\dsyzranov\\Desktop\\Django\\djsite\\venv\\Scripts"))
+print(os.system("cd C:\\Users\\dsyzranov\\Desktop\\Django\\djsite\\venv\\Scripts\\Activate.ps1"))
--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", index, name="home"),
     path("cats/<int:catid>/", categories),
     re_path(r"^archive/(?P<year>[0-9]{4})", archive),
+    path("ololo", index),
 ]
