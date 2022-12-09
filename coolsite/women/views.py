54b5c8bce3c5d03d127351fefd83bf9a9bc18696
DmitryLurk <redalert337596@gmail.com>
2022-12-09 14:31:03 +0400

    simple tags

--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -22,10 +22,8 @@ menu = [{"title": 'Обсайт', "url_name": "about"},
 
 def index(request):
     posts = Women.objects.all()
-    cats = Category.objects.all()
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Главная',
         'cat_selected': 0
@@ -63,14 +61,12 @@ def show_post(request, post_id):
 
 def show_category(request, cat_id):
     posts = Women.objects.filter(cat_id=cat_id)
-    cats = Category.objects.all()
 
     if len(posts) == 0:
         raise Http404
 
     context = {
         'posts': posts,
-        'cats': cats,
         'menu': menu,
         'title': 'Рубрики',
         'cat_selected': cat_id
